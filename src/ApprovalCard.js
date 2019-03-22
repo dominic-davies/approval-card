f42536ef1fa40c12eec9226c7ffd813d7ee73468
Dominic Davies <domdaviesit@gmail.com>
2019-03-22 20:24:03 +0000

    create approval card component and show in app

---
+++ src/ApprovalCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const ApprovalCard = () => {
+    return (
+        <div className="ui card">
+            <div className="content">
+            {/* <img className="right floated mini ui image" src="/images/avatar/large/elliot.jpg" /> */}
+            <div className="header">
+                Elliot Fu
+            </div>
+            <div className="meta">
+                Friends of Veronika
+            </div>
+            <div className="description">
+                Elliot requested permission to view your contact details
+            </div>
+            </div>
+            <div className="extra content">
+                <div className="ui two buttons">
+                    <div className="ui basic green button">Approve</div>
+                    <div className="ui basic red button">Reject</div>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default ApprovalCard;
