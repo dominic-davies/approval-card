b24bbb358dcba67acdb3148916c484d3a0066216
Dominic Davies <domdaviesit@gmail.com>
2019-03-22 21:01:20 +0000

    add another approval card and show children inside of that child component - just pass in some arbitrary text

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ const App = () => {
     return (
         <div className="ui container comments">
             <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
                 <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="hello world" avatar={faker.image.avatar()} />
             </ApprovalCard>
             <ApprovalCard>
