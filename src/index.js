9efcc68bfe2b8b52fd99e5605d51af3dc2399ae2
Dominic Davies <domdaviesit@gmail.com>
2019-03-22 12:59:32 +0000

    add comment using naive approach

--- src/index.js
+++ src/index.js
@@ -2,7 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div>Hello world</div>
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img src="/" alt="avatar"/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Stephen
+                    </a>
+                    <div className="metada">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis dignissimos dicta velit dolorem non perferendis!</div>
+                </div>
+            </div>
+        </div>
+    );
+    
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'))
