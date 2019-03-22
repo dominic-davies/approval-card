eaa1447e8b77f6025f1baf1acc739210d14a8d4a
Dominic Davies <domdaviesit@gmail.com>
2019-03-22 16:56:15 +0000

    pass image from parent to child component as a prop

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
-// Single component
+// Single component with 3 instances of CommentDetail
 const App = () => {
     return (
         <div className="ui container comments">
             
-            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="hello world" />
-            <CommentDetail author="Alex" timeAgo="Yesterday at 6:45PM" content="hello world again" />
-            <CommentDetail author="Jane" timeAgo="Today at 9:45PM" content="hello world one more time" />
+            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="hello world" avatar={faker.image.avatar()} />
+            <CommentDetail author="Alex" timeAgo="Yesterday at 6:45PM" content="hello world again" avatar={faker.image.avatar()} />
+            <CommentDetail author="Jane" timeAgo="Today at 9:45PM" content="hello world one more time" avatar={faker.image.avatar()} />
         </div>
     );
     
